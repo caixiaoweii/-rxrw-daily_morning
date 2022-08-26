f9b6d22ec3e37c526e4bbe9a85126cd2d529fcc1
caixiaoweii <71826654+caixiaoweii@users.noreply.github.com>
2022-08-27 00:02:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  high = '最高' + str(Math.floor(weather['high'])) + '℃'
-  low = '最低' + str(Math.floor(weather['low'])) + '℃'
+  high = str(Math.floor(weather['high'])) + '℃'
+  low =  str(Math.floor(weather['low'])) + '℃'
   return weather['weather'], math.floor(weather['temp']), high, low
 
 def get_count():
